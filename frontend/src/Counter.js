1b37f34549f0531b126956497d58d4c52fddf9c2
Sabelle Huang <sabelle2@illinois.edu>
2019-09-15 23:45:16 -0500

    part 2 progress

--- frontend/src/Counter.js
+++ frontend/src/Counter.js
@@ -2,14 +2,21 @@ import React, { Component } from 'react'
 
 class Counter extends Component {
   // YOUR CODE GOES BELOW
-  count: 0;
-
+  int count = 0;
+  incrClick = () => {
+    count++;
+    alert('count has been incremented')
+  }
+  decrClick = () => {
+    count--;
+    alert('count has been decremented')
+  }
   render() {
     return (
       <div>
         <label>{count}</label>
-        <button>increment</button>
-        <button>decrement</button>
+        <button onClick={this.incrClick}>increment</button>
+        <button onClick={this.decrClick}>decrement</button>
       <div/>
     )
   }
